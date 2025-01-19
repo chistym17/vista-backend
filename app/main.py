3ea7f20db41bb23dbfc11d62d11949ea79ddd460
chistym17 <mchisty31@gmail.com>
2025-01-19 22:27:48 +0600

    db connection added

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
+
 app = FastAPI(title="FastAPI MongoDB App")
 
+@app.on_event("startup")
+async def startup_db_client():
+    app.mongodb_client = AsyncIOMotorClient(settings.mongodb_uri)
+    app.mongodb = app.mongodb_client.vista 
 
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    app.mongodb_client.close()
 
 @app.get("/")
 async def root():
